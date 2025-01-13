bfcc314cccab733a60c1fc23771b90e83c1128c5
KobayashiComputing <andy@kobayashicomputing.com>
2025-01-13 17:10:43 -0500

    Added functions to count words and character frequencies, and to print a pretty report.

--- main.py
+++ main.py
@@ -3,9 +3,30 @@ def word_count(book_contents):
 
     return len(words)
 
+def char_count(book_contents):
+    letter_counts = {
+        "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0,
+        "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
+    }
+    lowercase_contents = book_contents.lower()
+    for letter in lowercase_contents:
+        if letter in letter_counts:
+            current_count = letter_counts[letter]
+            letter_counts[letter] = current_count + 1
+    
+    return dict(sorted(letter_counts.items(), key=lambda item: item[1], reverse=True))
+
+
 
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
 # print(file_contents)
-print(word_count(file_contents))
+print("--- Begin report of books/frankenstein.txt ---")
+print(word_count(file_contents), "words found in the document\n")
+# print(char_count(file_contents))
+char_freq = char_count(file_contents)
+for char in char_freq:
+    print(f"The '{char}' character was found {char_freq[char]} times")
+
+print("--- End report ---")
